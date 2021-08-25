e458e4e5002ef6a288e61686161c8e6bbc495083
Emma-90844 <enabojohn@gmail.com>
2021-08-25 20:38:00 +0300

    Created Cards, Chart, CountryPicker Components and called Covid.Madro API in App

---
+++ src/api/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import reactDom from 'react-dom';
+import axios from 'axios';
+const url = 'https://covid19.mathdro.id/api';
+
+
+export const fetchData = async() => {
+    try {
+        const response  = await axios.get(url);
+        return response;
+        
+    } catch (error){
+
+    }
+    
+    
+}
+
