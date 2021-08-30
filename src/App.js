9745c695ffe03115534f0510e3c4de078f020c5c
Emma-90844 <enabojohn@gmail.com>
2021-08-30 16:08:24 +0300

    Set chart data as a prop in the Chard component in App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import {Cards, Chart,CountryPicker} from './components'
 import styles from './App.module.css';
-import { fetchData } from './api'
-
+import { fetchData } from './api';
+import COVID from './images/covid.png';
 
 
 class App extends React.Component {
@@ -14,29 +14,28 @@ class App extends React.Component {
 
     async componentDidMount(){ 
         const fetchedData = await fetchData();
-        this.setState({ data:fetchedData})
+        this.setState({ data: fetchedData});
     }
 
-handleCountryChange = async (country) => {
-    console.log(country)
-    //fetchData
-    //setTheState
+    handleCountryChange = async (country) => {
+        //fetchData
+        const fetchedData = await fetchData(country);
+       
+        this.setState({ data:fetchedData, country: country })
+        //setTheState
 }
 
-
-
-
-
     render(){
-        const { data } = this.state;
+        const { data, country } = this.state;
         return (
             <div className={styles.container}>
+                <img  className={styles.image} alt="COVID-19" src={COVID}/>
                 <Cards data={ data }/>
                 <CountryPicker handleCountryChange={this.handleCountryChange}/>
-                <Chart/>
+                <Chart data={data} country={country}/>
             </div>
         )
     }
 }
-
+ 
 export default App;
